9786417c64470dde7798041586d703415707f388
Mason Capps <masoncapps42119@gmail.com>
2022-07-20 16:37:14 -0500

    Adds solution

---
+++ JavaScript/squares-of-a-sorted-array.js
@@ -0,0 +1,21 @@
+var sortedSquares = function(nums) {
+  let squaredArray = [];
+  nums.forEach(function(number) {
+    squaredArray.push(number * number);
+  });
+  let count = 0;
+  for (let i = 0; i < squaredArray.length; i++) {
+    if (squaredArray[i] >= squaredArray[i + 1]) {
+      let elementSwapper = squaredArray[i]
+      squaredArray[i] = squaredArray[i + 1];
+      squaredArray[i + 1] = elementSwapper;
+    } else {
+      i = -1;
+      count++;
+    }
+    if (count > squaredArray.length) {
+      break;
+    }
+  }
+  return squaredArray;
+};
