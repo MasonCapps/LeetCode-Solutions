b834ce99e34a3688ba2adad2d330fbb4d535d1f4
Mason Capps <masoncapps42119@gmail.com>
2022-07-20 14:43:48 -0500

    Adds solution

--- JavaScript/binary-search.js
+++ JavaScript/binary-search.js
@@ -2,7 +2,7 @@ var search = function(nums, target) {
   let low = 0;
   let high = nums.length - 1;
   while (low <= high) {
-    let middle = Math.floor((low + high) / 2);
+    let middle = parseInt((low + high) / 2);
     if (nums[middle] === target) {
       return middle;
     } else if (nums[middle] < target) {
