2c5a1ca77b5432891a569df5cc00a021246c0ddb
Mason Capps <masoncapps42119@gmail.com>
2022-07-21 00:03:46 -0500

    Adds solution

--- JavaScript/squares-of-a-sorted-array.js
+++ JavaScript/squares-of-a-sorted-array.js
@@ -6,7 +6,7 @@ var sortedSquares = function(nums) {
   let count = 0;
   for (let i = 0; i < squaredArray.length; i++) {
     if (squaredArray[i] >= squaredArray[i + 1]) {
-      let elementSwapper = squaredArray[i]
+      let elementSwapper = squaredArray[i];
       squaredArray[i] = squaredArray[i + 1];
       squaredArray[i + 1] = elementSwapper;
     } else if (count > squaredArray.length) {
