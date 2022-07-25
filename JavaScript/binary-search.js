1c62bade0cf2b5cddbdd5e0b583d023c53c7f4d8
Mason Capps <masoncapps42119@gmail.com>
2022-07-24 20:38:07 -0500

    Adds solution

--- JavaScript/binary-search.js
+++ JavaScript/binary-search.js
@@ -1,14 +1,15 @@
 var search = function(nums, target) {
   let low = 0;
   let high = nums.length - 1;
+  
   while (low <= high) {
     let middle = parseInt((low + high) / 2);
-    if (nums[middle] === target) {
-      return middle;
-    } else if (nums[middle] < target) {
+    if (nums[middle] < target) {
       low = middle + 1;
     } else if (nums[middle] > target) {
       high = middle - 1;
+    } else {
+      return middle;
     }
   }
   return -1;
