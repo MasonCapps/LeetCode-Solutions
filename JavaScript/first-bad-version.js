23184bdd0a5e7477857d898a8fde9beb1fc23ca9
Mason Capps <masoncapps42119@gmail.com>
2022-07-24 21:23:51 -0500

    Adds solution

--- JavaScript/first-bad-version.js
+++ JavaScript/first-bad-version.js
@@ -2,6 +2,7 @@ var solution = function(isBadVersion) {
   return function(n) {
     let low = 1;
     let high = n;
+    
     while (low < high) {
       let middle = parseInt((low + high) / 2);
       if (isBadVersion(middle)) {
