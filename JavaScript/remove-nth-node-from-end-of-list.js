1e29a5a4efdd4ac24d8f49dd8eb59b6bcf801904
Mason Capps <masoncapps42119@gmail.com>
2022-07-25 14:06:46 -0500

    Adds solution

---
+++ JavaScript/remove-nth-node-from-end-of-list.js
@@ -0,0 +1,21 @@
+var removeNthFromEnd = function(head, n) {
+  let dummyHead = new ListNode(-1);
+  dummyHead.next = head;
+  let result = dummyHead;
+  let tail = head;
+  
+  while (n--) {
+    tail = tail.next;
+  }
+  
+  let curr = head;
+  let prev = dummyHead;
+  while (tail) {
+    tail = tail.next;
+    curr = curr.next;
+    prev = prev.next;
+  }
+  
+  prev.next = curr.next;
+  return result.next;
+};
