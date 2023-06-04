482ae41f51bf8c65a5d14c7b5ddadc066123cb3c
Mason Capps <masoncapps42119@gmail.com>
2023-06-04 13:32:47 -0500

    Adds solution

---
+++ Python/two-sum.py
@@ -0,0 +1,8 @@
+from ast import List
+
+def twoSum(self, nums: List[int], target: int) -> List[int]:
+  nums_hash = {}
+  for i in range(len(nums)):
+    if target - nums[i] in nums_hash:
+      return nums_hash[target - nums[i]], i
+    nums_hash[nums[i]] = i
