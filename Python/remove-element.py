72b6ad78273ce5d2dde12590df3cec5b1121d262
Mason Capps <masoncapps42119@gmail.com>
2023-06-10 11:54:06 -0500

    Adds solution

---
+++ Python/remove-element.py
@@ -0,0 +1,9 @@
+from ast import List
+
+def removeElement(self, nums: List[int], val: int) -> int:
+  while True:
+    if val in nums:
+      nums.remove(val)
+    else:
+      break
+  return len(nums)
